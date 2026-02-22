83a2a532262099c9a873c307ccbdceebb4106d98
Robin <114897657+rvHoney@users.noreply.github.com>
2026-02-22 20:31:06 +0100

    Feat/network core (#1)
    * feat(config): flag-based TCP server configuration
    
    * feat(server): tcp listener & connection acceptation loop
    
    * feat(logger): structured logging and integration
    
    * feat(server): enhance command parsing + tests
    
    * chore(repo): makefile based build system
    
    * feat(database): add thread safe key-value store
    
    * feat(server): inject database dependency to server
    
    * feat(database): adding Delete to database to remove keys
    
    * feat(server): command execution for SET, GET and DEL
    
    * feat(server): check returns status on connection close and command response
    
    * chore(ci): adding 'golangci-lint' configuration
    
    * chore(ci): adding a CI for Build, Lint, Test and Coverage
    
    * docs(repo): adding a README to enhance project documentation and onboarding
    
    * fix(ci): removing golangci-lint because go 1.25 is not supported

---
+++ internal/database/database_test.go
@@ -0,0 +1,159 @@
+package database
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+func TestGet(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    map[string]string
+		key     string
+		wantVal string
+		wantOk  bool
+	}{
+		{
+			name:    "Existing Key",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field1",
+			wantVal: "value1",
+			wantOk:  true,
+		},
+		{
+			name:    "Not Found",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field4'",
+			wantVal: "",
+			wantOk:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := New()
+			db.data = tt.data
+
+			val, ok := db.Get(tt.key)
+			if val != tt.wantVal {
+				t.Errorf("Get() val = %v, wantVal %v", val, tt.wantVal)
+			}
+			if ok != tt.wantOk {
+				t.Errorf("Get() ok = %v, wantOk %v", ok, tt.wantOk)
+			}
+		})
+	}
+}
+
+func TestSet(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    map[string]string
+		key     string
+		wantVal string
+	}{
+		{
+			name:    "Already Exist",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field1",
+			wantVal: "value1",
+		},
+		{
+			name:    "New Key",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field4",
+			wantVal: "field4",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := New()
+			db.data = tt.data
+
+			db.Set(tt.key, tt.wantVal)
+
+			val, ok := tt.data[tt.key]
+			if val != tt.wantVal {
+				t.Errorf("Get() val = %v, wantVal %v", val, tt.wantVal)
+			}
+			if ok != true {
+				t.Errorf("Get() ok = %v, want %v", ok, true)
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	tests := []struct {
+		name    string
+		data    map[string]string
+		key     string
+		wantMap map[string]string
+	}{
+		{
+			name:    "Existing Key",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field2",
+			wantMap: map[string]string{"field1": "value1", "field3": "value3"},
+		},
+		{
+			name:    "Unknow Key",
+			data:    map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+			key:     "field4",
+			wantMap: map[string]string{"field1": "value1", "field2": "value2", "field3": "value3"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := New()
+			db.data = tt.data
+
+			db.Delete(tt.key)
+
+			val, ok := tt.data[tt.key]
+			wantVal, wantOk := tt.wantMap[tt.key]
+			if val != wantVal {
+				t.Errorf("Get() val = %v, wantVal %v", val, tt.wantMap[tt.key])
+			}
+			if ok != wantOk {
+				t.Errorf("Get() ok = %v, want %v", ok, wantOk)
+			}
+		})
+	}
+}
+
+func TestDatabaseConcurrency(t *testing.T) {
+	db := New()
+	const workers int = 50
+	const iterations int = 1000
+
+	var wg sync.WaitGroup
+	wg.Add(workers * 2)
+
+	// write test
+	for i := 0; i < workers; i++ {
+		go func(workerID int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				key := fmt.Sprintf("key-%d", j%10)
+				db.Set(key, "value")
+			}
+		}(i)
+	}
+
+	// read test
+	for i := 0; i < workers; i++ {
+		go func(workerID int) {
+			defer wg.Done()
+			for j := 0; j < iterations; j++ {
+				key := fmt.Sprintf("key-%d", j%10)
+				db.Get(key)
+			}
+		}(i)
+	}
+
+	wg.Wait()
+}
---
+++ internal/server/server.go
@@ -0,0 +1,118 @@
+// Package server handles TCP communications
+package server
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net"
+	"strings"
+
+	"github.com/rvHoney/kvgo/internal/config"
+	"github.com/rvHoney/kvgo/internal/database"
+)
+
+// executeCommand dispatches the command based on its name and run it.
+func executeCommand(db *database.Database, cmd string, args []string) string {
+	switch cmd {
+	case "SET":
+		if len(args) != 2 {
+			return "ERR wrong number of arguments for 'SET'\n"
+		}
+		db.Set(args[0], args[1])
+		return "OK\n"
+	case "GET":
+		if len(args) != 1 {
+			return "ERR wrong number of arguments for 'GET'\n"
+		}
+		val, ok := db.Get(args[0])
+		if !ok {
+			return "(nil)\n"
+		}
+		return val + "\n"
+	case "DEL":
+		if len(args) != 1 {
+			return "ERR wrong number of arguments for 'DEL'\n"
+		}
+		db.Delete(args[0])
+		return "OK\n"
+	default:
+		return fmt.Sprintf("ERR unknown command '%s'\n", cmd)
+	}
+}
+
+// parseCommand parses the user input into a command and its arguments.
+func parseCommand(input string) (string, []string, error) {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return "", nil, errors.New("empty command")
+	}
+
+	fields := strings.Fields(input)
+
+	return strings.ToUpper(fields[0]), fields[1:], nil
+}
+
+// handleConnection manages a TCP connection, reading and executing commands in a loop.
+func handleConnection(conn net.Conn, db *database.Database) {
+	defer func() {
+		if err := conn.Close(); err != nil {
+			slog.Debug("failed to close connection", "error", err)
+		}
+	}()
+	slog.Debug("new TCP connection", "remoteAddr", conn.RemoteAddr())
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				slog.Debug("packet reading fail", "error", err)
+			}
+			break
+		}
+
+		cmd, args, err := parseCommand(line)
+		if err != nil {
+			slog.Debug("packet parsing fail", "error", err)
+			continue
+		}
+
+		response := executeCommand(db, cmd, args)
+		slog.Debug("executed", "cmd", cmd, "args", args, "response", response)
+
+		_, err = fmt.Fprintln(conn, response)
+		if err != nil {
+			slog.Debug("failed to send response", "error", err)
+			break
+		}
+	}
+}
+
+// Start launches a TCP server according to the configuration
+func Start(cfg *config.Config, db *database.Database) error {
+	address := fmt.Sprintf("%s:%d", cfg.Host, cfg.Port)
+	ln, err := net.Listen("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err := ln.Close(); err != nil {
+			slog.Debug("failed to close listener", "error", err)
+		}
+	}()
+
+	slog.Info("TCP server is listening", "addr", ln.Addr().String())
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			slog.Debug("connection accept failed", "error", err)
+			continue
+		}
+
+		go handleConnection(conn, db)
+	}
+}
