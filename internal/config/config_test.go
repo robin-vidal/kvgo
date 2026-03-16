e09c262da4a546b4a1286890fdff8567d2510164
robin-vidal <robin.vidal.pro@gmail.com>
2026-03-16 14:59:03 +0100

    feat(config): adding flag for shard amount

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,25 +1,37 @@
 package config
 
 import (
+	"runtime"
 	"testing"
 )
 
 func TestParse(t *testing.T) {
 	tests := []struct {
-		name      string
-		args      []string
-		wantHost  string
-		wantPort  int
-		wantDebug bool
-		wantErr   bool
+		name            string
+		args            []string
+		wantHost        string
+		wantPort        int
+		wantShardAmount int
+		wantDebug       bool
+		wantErr         bool
 	}{
 		{
-			name:      "Default values",
-			args:      []string{},
-			wantHost:  "localhost",
-			wantPort:  6379,
-			wantDebug: false,
-			wantErr:   false,
+			name:            "Default values",
+			args:            []string{},
+			wantHost:        "localhost",
+			wantPort:        6379,
+			wantShardAmount: runtime.NumCPU(),
+			wantDebug:       false,
+			wantErr:         false,
+		},
+		{
+			name:            "Zero Shard",
+			args:            []string{"--shardAmount=0"},
+			wantHost:        "localhost",
+			wantPort:        6379,
+			wantShardAmount: 0,
+			wantDebug:       false,
+			wantErr:         true,
 		},
 	}
 
@@ -34,16 +46,14 @@ func TestParse(t *testing.T) {
 				if cfg.Host != tt.wantHost {
 					t.Errorf("Parse() error = %v, wantHost %v", cfg.Host, tt.wantHost)
 				}
-			}
-			if !tt.wantErr {
-				if cfg.Host != tt.wantHost {
-					t.Errorf("Parse() error = %v, wantHost %v", cfg.Host, tt.wantHost)
-				}
 				if cfg.Port != tt.wantPort {
 					t.Errorf("Parse() error = %v, wantPort %v", cfg.Port, tt.wantPort)
 				}
 				if cfg.Debug != tt.wantDebug {
 					t.Errorf("Parse() error = %v, wantDebug %v", cfg.Debug, tt.wantDebug)
+				}
+				if cfg.ShardAmount != tt.wantShardAmount {
+					t.Errorf("Parse() error = %v, wantShardAmount %v", cfg.ShardAmount, tt.wantShardAmount)
 				}
 			}
 		})
