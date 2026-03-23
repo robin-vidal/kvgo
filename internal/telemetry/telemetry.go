6a6b50ba6a5e198116dbe1642b9e47e3fea1d84c
robin-vidal <robin.vidal.pro@gmail.com>
2026-03-23 15:58:56 +0100

    feat(telemetry): swap stdout exporter for Prometheus

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -2,17 +2,18 @@ package telemetry
 
 import (
 	"context"
-	"time"
+	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
+	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 )
 
 func Init() (shutdown func(context.Context) error, err error) {
-	exporter, err := stdoutmetric.New()
+	exporter, err := prometheus.New()
 	if err != nil {
 		return nil, err
 	}
@@ -22,17 +23,17 @@ func Init() (shutdown func(context.Context) error, err error) {
 		semconv.ServiceName("kvgo"),
 	)
 
-	reader := metric.NewPeriodicReader(
-		exporter,
-		metric.WithInterval(10*time.Second),
-	)
-
 	provider := metric.NewMeterProvider(
 		metric.WithResource(res),
-		metric.WithReader(reader),
+		metric.WithReader(exporter),
 	)
 
 	otel.SetMeterProvider(provider)
+
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":9090", nil)
+	}()
 
 	return provider.Shutdown, nil
 }
