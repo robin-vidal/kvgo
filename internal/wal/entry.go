3dc7940e998ad7d1e927fbc5031b93a01e89bbed
Robin <114897657+robin-vidal@users.noreply.github.com>
2026-06-10 21:56:03 +0200

    feat(wal): encoding/decoding functions with tests (#48)
    * feat(wal): encoding/decoding functions with tests
    
    * feat(wal): adding bound checks safeguards

---
+++ internal/wal/entry.go
@@ -0,0 +1,146 @@
+package wal
+
+import (
+	"bytes"
+	"encoding/binary"
+	"errors"
+	"hash/crc32"
+)
+
+const (
+	commandMaxLen  int = 255
+	keyMaxLen      int = 65535
+	valueMaxLen    int = 65535
+	seqNumSize     int = 8 // uint64
+	commandLenSize int = 1 // uint8
+	keyLenSize     int = 2
+	valueLenSize   int = 2
+	checksumSize   int = 4 // uint32
+)
+
+type Entry struct {
+	SeqNum  uint64
+	Command string
+	Key     string
+	Value   *string
+}
+
+func Encode(e Entry) ([]byte, error) {
+	if len(e.Command) > commandMaxLen {
+		return nil, errors.New("command too long")
+	}
+	if len(e.Key) > keyMaxLen {
+		return nil, errors.New("key too long")
+	}
+	if e.Value != nil && len(*e.Value) > valueMaxLen {
+		return nil, errors.New("value too long")
+	}
+
+	var buf bytes.Buffer
+
+	binary.Write(&buf, binary.BigEndian, e.SeqNum)
+
+	buf.WriteByte(byte(len(e.Command)))
+	buf.WriteString(e.Command)
+
+	binary.Write(&buf, binary.BigEndian, uint16(len(e.Key)))
+	buf.WriteString(e.Key)
+
+	if e.Value == nil {
+		buf.WriteByte(0)
+	} else {
+		buf.WriteByte(1)
+		binary.Write(&buf, binary.BigEndian, uint16(len(*e.Value)))
+		buf.WriteString(*e.Value)
+	}
+
+	checksum := crc32.ChecksumIEEE(buf.Bytes())
+	binary.Write(&buf, binary.BigEndian, checksum)
+
+	return buf.Bytes(), nil
+}
+
+type reader struct {
+	data []byte
+	pos  int
+}
+
+func (r *reader) bytes(n int) ([]byte, error) {
+	if r.pos+n > len(r.data) {
+		return nil, errors.New("entry too short")
+	}
+	b := r.data[r.pos : r.pos+n]
+	r.pos += n
+	return b, nil
+}
+
+func Decode(b []byte) (Entry, error) {
+	if len(b) < checksumSize {
+		return Entry{}, errors.New("entry too short")
+	}
+
+	data := b[:len(b)-checksumSize]
+	storedChecksum := binary.BigEndian.Uint32(b[len(data):])
+
+	if crc32.ChecksumIEEE(data) != storedChecksum {
+		return Entry{}, errors.New("checksum mismatch")
+	}
+
+	r := &reader{data: data}
+
+	b, err := r.bytes(seqNumSize)
+	if err != nil {
+		return Entry{}, err
+	}
+	seqNum := binary.BigEndian.Uint64(b)
+
+	b, err = r.bytes(commandLenSize)
+	if err != nil {
+		return Entry{}, err
+	}
+	commandLen := int(b[0])
+
+	b, err = r.bytes(commandLen)
+	if err != nil {
+		return Entry{}, err
+	}
+	command := string(b)
+
+	b, err = r.bytes(keyLenSize)
+	if err != nil {
+		return Entry{}, err
+	}
+	keyLen := int(binary.BigEndian.Uint16(b))
+
+	b, err = r.bytes(keyLen)
+	if err != nil {
+		return Entry{}, err
+	}
+	key := string(b)
+
+	b, err = r.bytes(1)
+	if err != nil {
+		return Entry{}, err
+	}
+	present := int(b[0])
+
+	var value *string
+	if present == 1 {
+		b, err = r.bytes(valueLenSize)
+		if err != nil {
+			return Entry{}, err
+		}
+		valueLen := int(binary.BigEndian.Uint16(b))
+
+		b, err = r.bytes(valueLen)
+		if err != nil {
+			return Entry{}, err
+		}
+		s := string(b)
+		value = &s
+	} else if present != 0 {
+		return Entry{}, errors.New("invalid present byte")
+	}
+
+	return Entry{SeqNum: seqNum, Command: command, Key: key, Value: value}, nil
+}
