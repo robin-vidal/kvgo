ec17fe6a2c40016b9e74e28cfe51f99ea3218f6a
robin-vidal <robin.vidal.pro@gmail.com>
2026-06-23 14:32:34 +0200

    feat(wal): add Open, Close and Append to manage WAL storage

---
+++ internal/wal/wal.go
@@ -0,0 +1,54 @@
+package wal
+
+import (
+	"errors"
+	"os"
+	"sync"
+
+	"github.com/robin-vidal/kvgo/internal/config"
+)
+
+type WAL struct {
+	file *os.File
+	cfg  *config.WalConfig
+	mu   sync.Mutex
+}
+
+type Wal interface {
+	Append(e Entry) error
+	Close() error
+}
+
+func Open(cfg *config.WalConfig) (*WAL, error) {
+	file, err := os.OpenFile(cfg.WalPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	return &WAL{file: file, cfg: cfg}, nil
+}
+
+func (wal *WAL) Close() error {
+	return wal.file.Close()
+}
+
+func (wal *WAL) Append(e Entry) error {
+	wal.mu.Lock()
+	defer wal.mu.Unlock()
+
+	encoded, err := Encode(e)
+	if err != nil {
+		return err
+	}
+
+	n, err := wal.file.Write(encoded)
+	if err != nil {
+		return err
+	}
+
+	if n != len(encoded) {
+		return errors.New("partial write to WAL")
+	}
+
+	return wal.file.Sync()
+}
