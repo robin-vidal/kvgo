cbf7d8f61683e88407dde2e6143a77724a70d958
robin-vidal <robin.vidal.pro@gmail.com>
2026-06-26 12:24:22 +0200

    test(wal): adding test cases for wal config parsing

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -19,7 +19,7 @@ func TestParse(t *testing.T) {
 		wantErr                 bool
 	}{
 		{
-			name:                    "Default values",
+			name:                    "default values",
 			args:                    []string{},
 			wantHost:                "0.0.0.0",
 			wantPort:                6379,
@@ -31,7 +31,7 @@ func TestParse(t *testing.T) {
 			wantErr:                 false,
 		},
 		{
-			name:                    "Zero Shard",
+			name:                    "zero shard",
 			args:                    []string{"--shardAmount=0"},
 			wantHost:                "0.0.0.0",
 			wantPort:                6379,
@@ -41,6 +41,18 @@ func TestParse(t *testing.T) {
 			wantSnapshotPath:        "/var/lib/kvgo/snapshot.db",
 			wantCompactionThreshold: 10000,
 			wantErr:                 true,
+		},
+		{
+			name:                    "custom values",
+			args:                    []string{"--host=1.1.1.1", "--port=9999", "--debug=true", "--walPath=./wal.log", "--snapshotPath=./snapshot.db", "--compactionThreshold=1"},
+			wantHost:                "1.1.1.1",
+			wantPort:                9999,
+			wantShardAmount:         runtime.NumCPU(),
+			wantDebug:               true,
+			wantWalPath:             "./wal.log",
+			wantSnapshotPath:        "./snapshot.db",
+			wantCompactionThreshold: 1,
+			wantErr:                 false,
 		},
 	}
 
@@ -63,6 +75,15 @@ func TestParse(t *testing.T) {
 				}
 				if cfg.ShardAmount != tt.wantShardAmount {
 					t.Errorf("Parse() error = %v, wantShardAmount %v", cfg.ShardAmount, tt.wantShardAmount)
+				}
+				if cfg.WalConfig.WalPath != tt.wantWalPath {
+					t.Errorf("Parse() error = %v, wantWallPath %v", cfg.WalConfig.WalPath, tt.wantWalPath)
+				}
+				if cfg.WalConfig.SnapshotPath != tt.wantSnapshotPath {
+					t.Errorf("Parse() error = %v, wantSnapshotPath %v", cfg.WalConfig.SnapshotPath, tt.wantSnapshotPath)
+				}
+				if cfg.WalConfig.CompactionThreshold != tt.wantCompactionThreshold {
+					t.Errorf("Parse() error = %v, wantCompactionThreshold %v", cfg.WalConfig.CompactionThreshold, tt.wantCompactionThreshold)
 				}
 			}
 		})
